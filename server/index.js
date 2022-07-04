546f545d5c6ea89338f4dda12967f427b79e5b69
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-04 00:53:50 +0000

    Started api routes for team_members, jobs, and jobId.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 require('dotenv/config');
 const express = require('express');
+const pg = require('pg');
+const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
 const app = express();
-
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
 app.use(staticMiddleware);
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api/team_members', (req, res, next) => {
+
+});
+
+app.get('/api/jobs', (req, res, next) => {
+
+});
+
+app.post('/api/job/:jobId', (req, res, next) => {
+
+});
 
 app.use(errorMiddleware);
 
