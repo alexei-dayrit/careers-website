a5b55cb7f39800350af5328ab60aac75df069b33
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-06 03:59:17 +0000

    Created Navbar styling for desktop screens and imported material-ui icons.

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,7 +3,23 @@ import React, { useState } from 'react';
 const Navbar = () => {
 
   return (
-    <div>
+    <div className='fixed w-full flex justify-between p-4 items-center'>
+
+      {/* Logo */}
+      <h1 className='text-lg font-medium text-center'>
+        Careers at React
+      </h1>
+
+      <nav className='text-lg font-semibold p-6'>
+        <ul className='md:flex gap-8 bg-white/10'>
+          <li>About</li>
+          <li>Mission</li>
+          <li>Values</li>
+          <li>Our Team</li>
+          <li>Jobs</li>
+        </ul>
+      </nav>
+
     </div>
   );
 };
