7e996f41ae4e7de2860ea6ef44fa9bce4d197ffc
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-07 04:19:27 +0000

    Updated About component to have full page height and adjusted spacings.

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -4,20 +4,35 @@ const background = '/images/space-bg.jpeg';
 const About = () => {
   return (
     <>
-      <div style={{ backgroundImage: `url(${background})`, backgroundPosition: 'center' }}
-        className='mt-32 p-4 text-white'>
-        <h1 className='text-3xl text-center font-semibold'>The React Story</h1>
-      </div>
+      <div style={{ backgroundImage: `url(${background})` }}
+        className='mt-28 pt-20 text-white bg-center bg-contain h-screen w-full px-10'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-semibold pb-3'>The React Story</h1>
+          <p>Free and open source.</p>
+        </div>
 
-      <div className='bg-[#f8f9fa] p-4'>
-        <p className='text-lg py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est inventore
-          non quasi doloremque reprehenderit consequatur at exercitationem dolorum s
-          imilique eveniet!
-        </p>
-
-        <p className='text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo veritatis
-          autem cumque odit quo distinctio quia sapiente praesentium sequi architecto.
-        </p>
+        <div className='p-4 text-lg leading-loose'>
+          <p className='py-4 mt-8'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptate
+            id fuga quas magni, et laborum blanditiis laboriosam modi? Est ullam exercitationem
+            qui harum, atque, debitis hic officia voluptas repellat ducimus minus mollitia
+            molestiae autem enim doloremque optio voluptates nulla, ab quis itaque reiciendis
+            aliquid! Dicta facere voluptatem eius tempora illum? Ea dolor veritatis aliquam
+            dicta, dolorum nostrum nisi quaerat, itaque magnam nulla pariatur facilis ipsum
+            incidunt quidem, quis officiis aut quia ratione quibusdam molestiae in cum minus.
+            Quidem tempore, ad nihil nesciunt perferendis fugit aperiam placeat esse nisi quas,
+            nulla blanditiis rem commodi tempora hic, magni dolorem minima est.
+          </p>
+          <p className='pt-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odio ut,
+            repellat aut debitis quod ipsum officia harum fuga aperiam quam dolorem,
+            neque, deleniti officiis? Quaerat, laudantium blanditiis! Id omnis a ratione
+            nostrum enim recusandae assumenda magnam nobis temporibus, fuga voluptas
+            sit aut explicabo voluptatibus eum, itaque facilis repudiandae numquam
+            deserunt adipisci accusamus eligendi aspernatur? Impedit aperiam ut
+            consequuntur doloremque veniam.
+          </p>
+        </div>
       </div>
     </>
   );
