8e42139cbd9a3523a810c5e02cf85a12f3e28a8e
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-07 02:00:06 +0000

    Added mobile responsiveness to navbar with burger menu.

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
+import CompanyLogo from './assets/company-logo';
+import { MenuOutlined, Close } from '@material-ui/icons';
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const showMenu = () => {
+    setActive(!active);
+  };
 
   return (
     <div className='fixed w-full flex justify-between p-4 items-center'>
 
-      {/* Logo */}
-      <h1 className='text-lg font-medium text-center'>
-        Careers at React
+      <h1 className='flex justify-center items-center'>
+        <CompanyLogo />
+        <span className='text-lg font-medium pl-1'>Careers at React</span>
       </h1>
 
       <nav className='text-lg font-semibold p-6'>
-        <ul className='md:flex gap-8 bg-white/10'>
+        <div className='absolute right-6 md:hidden top-8 scale-150'>
+          <MenuOutlined onClick={showMenu} className='scale-125 cursor-pointer' />
+        </div>
+
+        <ul className={
+          active
+            ? 'flex-col flex items-center fixed inset-0 left-1/3 uppercase bg-black/40 backdrop-blur-lg gap-8 justify-center p-8 md:hidden'
+            : 'hidden md:flex gap-8 bg-white /10'}>
+
+          {active && <Close onClick={showMenu} className='text-red-700 z-50 scale-150 cursor-pointer' />}
           <li>About</li>
           <li>Mission</li>
           <li>Values</li>
