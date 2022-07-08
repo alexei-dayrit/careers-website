8484473a35fedf25668ef79e5906f6bfbce620b9
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-08 00:46:56 +0000

    Completed Values component.

--- client/components/Values.jsx
+++ client/components/Values.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 
+const values = [
+  {
+    id: '01',
+    img: '/images/composable.png',
+    title: 'Composable',
+    text: 'Custom components allow maintainable and flexible code. No more thousands of lines of HTML!'
+  },
+  {
+    id: '02',
+    img: '/images/fast&efficient.png',
+    title: 'Fast & Efficient',
+    text: 'Pre-built patterns and functions act as building blocks to create fast and scalable projects.'
+  },
+  {
+    id: '03',
+    img: '/images/easy.jpeg',
+    title: 'Simplicity',
+    text: 'JSX is a special syntax which make JS and HTML easier to grasp.'
+  }
+];
+
+const ValueItem = ({ value, text, img }) => {
+  return (
+    <div className='flex items-center flex-col h-72 md:h-96 w-full md:w-[30%] gap-4'>
+      <img src={img} alt='value image'
+        className={`w-48 h-48 object-cover
+          ${img === '/images/composable.png' && 'border-2'}`
+        } />
+      <h1 className='text-xl italic font-semibold'>{value}</h1>
+      <p className='text-center'>{text}</p>
+    </div>
+  );
+};
+
 const Values = () => {
   return (
-    <div className='h-screen w-full pt-16'>
-      <h1 className='text-4xl font-semibold pb-3'>Core Values</h1>
+    <div className='h-[80vh] w-full pt-16 px-4'>
+      <div className=''>
+        <h1 className='block text-4xl font-semibold text-center'>
+          Core Values
+        </h1>
+      </div>
+
+      <div className='flex flex-wrap items-center justify-center gap-8 h-[60vh]'>
+        {values.map(value =>
+          <ValueItem key={value.id} id={value.id} value={value.title}
+            text={value.text} img={value.img} />
+        )}
+      </div>
     </div>
   );
 };
