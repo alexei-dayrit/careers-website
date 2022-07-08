9b4cd1afd311ad2a76115fe38cc2726ddd662cde
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-08 03:11:31 +0000

    Added more dummy data for jobs

--- client/components/Jobs.jsx
+++ client/components/Jobs.jsx
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 const Jobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch('/api/jobs');
+        const result = await response.json();
+        setJobs(result);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <div className='min-h-screen bg-[#fafafa] py-6'>
       <div className='text-center'>
@@ -8,7 +24,9 @@ const Jobs = () => {
         <p>Start a career with us!</p>
       </div>
       <div>
-
+        {jobs.map(job =>
+          <div key={job.jobId}>{job.title}</div>
+        )}
       </div>
     </div>
   );
