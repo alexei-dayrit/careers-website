2f552c0414e790b91163066e98e20c28db682b42
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-11 22:04:03 +0000

    Fixed styling for navbar and jobs to make it feel more responsive for user.

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -19,37 +19,42 @@ const Navbar = () => {
   };
 
   return (
-    <div className='sticky bg-white top-0 w-full flex justify-between p-4 items-center'>
+    <div className='sticky text-white bg-black/90 top-0 w-full flex justify-between p-4 items-center'>
 
-      <h1 className='flex justify-center items-center'>
-        <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
+      <h1>
+        <Link to="about" spy={true} smooth={true} offset={-100} duration={500}
+          className='flex justify-center items-center cursor-pointer'>
           <CompanyLogo alt='Company Logo' />
+          <span className='text-lg font-medium pl-1'>Careers at React</span>
         </Link>
-        <span className='text-lg font-medium pl-1'>Careers at React</span>
       </h1>
 
       <nav className='text-lg font-semibold'>
-        <div className='absolute right-6 md:hidden top-8 scale-150'>
-          <MenuOutlined onClick={showMenu} className='scale-125 cursor-pointer' />
+        <div className='absolute right-6 md:hidden top-8 scale-150 z-50'>
+
+          {!active
+            ? <MenuOutlined onClick={showMenu} className='scale-125 cursor-pointer' />
+            : <Close onClick={showMenu} className='text-red-500 scale-125 cursor-pointer' />
+          }
         </div>
 
-        <ul className={
-          active
-            ? 'flex-col flex items-center fixed inset-0 left-1/3 uppercase text-white bg-black/40 backdrop-blur-lg gap-8 justify-center p-8 md:hidden'
-            : 'hidden md:flex gap-8'}>
-
-          {active && <Close onClick={showMenu} className='text-red-700 z-50 scale-125 cursor-pointer' />}
+        <ul className={`gap-8
+          ${active
+            ? `flex-col z-10 flex items-center justify-center fixed inset-0 left-1/3 uppercase
+            text-white bg-black/40 backdrop-blur-lg p-8 md:hidden`
+            : 'hidden md:flex'}`
+        }>
 
           {navItems.map((item, index) => (
             <li key={index} className='cursor-pointer'>
-              <Link to={item.link} smooth={true} offset={-75} duration={500}>
+              <Link to={item.link} smooth={true} offset={-75} duration={400}
+                className='hover:text-gray-500'>
                 {item.name}
               </Link>
             </li>
           ))}
         </ul>
       </nav>
-
     </div>
   );
 };
