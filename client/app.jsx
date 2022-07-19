1d0004181ed0ede4c134c2c7b550d0f12f63ebb3
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-19 18:40:45 +0000

    Changed app component to functional component

--- client/app.jsx
+++ client/app.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className='sm:container md:container lg:container mx-auto'>
-        <Navbar />
-        <Home />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='sm:container md:container lg:container mx-auto'>
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default App;
